9ffacbfd6ba8f02d8e6457331ae6b52d553b71c7
trackstar192 <zwaterso@nd.edu>
2015-01-16 22:21:22 -0500

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Zach Waterson on 1/16/15.
+//  Copyright (c) 2015 Zach Waterson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
